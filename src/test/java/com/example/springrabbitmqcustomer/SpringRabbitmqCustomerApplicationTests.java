c6dce03e1bac5a89cd58e7325dd00d2cb102bd97
ducuzi2710 <buisyduc2710@gmail.com>
2023-01-10 13:41:29 +0700

    first commit

---
+++ src/test/java/com/example/springrabbitmqcustomer/SpringRabbitmqCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrabbitmqcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
